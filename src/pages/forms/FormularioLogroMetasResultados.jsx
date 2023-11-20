225167f5892f87db1a83ab36661ad0bc507d6b80
hensahi <hency.lara@gmail.com>
2023-11-20 18:16:19 -0400

    Se crea estructura componente principal del formulario con input de prueba

--- src/pages/forms/FormularioLogroMetasResultados.jsx
+++ src/pages/forms/FormularioLogroMetasResultados.jsx
@@ -1,18 +1,29 @@
 import React from "react";
-import { Container, Typography } from "@mui/material";
+import { Container, TextField, Typography, Grid } from "@mui/material";
 
 function FormularioLogroMetasResultados() {
   return (
-    <Container>
-      <Typography
-        variant="h1"
-        sx={{
-          textAlign: "center",
-        }}
-      >
-        Logro de Metas y Resultados
-      </Typography>
-    </Container>
+    <>
+      <Container>
+        <Typography
+          variant="h1"
+          sx={{
+            textAlign: "center",
+          }}
+        >
+          Logro de Metas y Resultados
+        </Typography>
+      </Container>
+      <form>
+        <Grid Container>
+          <Container>
+            <Grid item>
+              <TextField label="Test" placeholder="Esto es una prueba" />
+            </Grid>
+          </Container>
+        </Grid>
+      </form>
+    </>
   );
 }
 
