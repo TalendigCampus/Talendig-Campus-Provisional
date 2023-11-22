a06b8819733ee93ad5f15152505de0ee67d6c380
hensahi <hency.lara@gmail.com>
2023-11-22 00:21:39 -0400

    se crea funcionalidad multi-step

--- src/pages/forms/FormularioLogroMetasResultados.jsx
+++ src/pages/forms/FormularioLogroMetasResultados.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import { Container, Typography, Grid } from "@mui/material";
 
 import "./forms_styles/FormularioLogroMetasResultadosStyles.css";
@@ -6,8 +7,23 @@ import "./forms_styles/FormularioLogroMetasResultadosStyles.css";
 import LogrosPage1 from "./LogrosPage1";
 import LogrosPage2 from "./LogrosPage2";
 import LogrosPage3 from "./LogrosPage3";
+import LogrosPage4 from "./LogrosPage4";
 
 function FormularioLogroMetasResultados() {
+  const [page, setPage] = useState(0);
+
+  const handlePage = () => {
+    if (page === 0) {
+      return <LogrosPage1 />;
+    } else if (page === 1) {
+      return <LogrosPage2 />;
+    } else if (page === 2) {
+      return <LogrosPage3 />;
+    } else {
+      return <LogrosPage4 />;
+    }
+  };
+
   return (
     <>
       <Container>
@@ -23,9 +39,7 @@ function FormularioLogroMetasResultados() {
         <hr className="hr" />
       </Container>
       <form>
-        <Grid Container>
-          <LogrosPage3 />
-        </Grid>
+        <Grid Container>{handlePage()}</Grid>
       </form>
     </>
   );
---
+++ src/pages/forms/LogrosPage4.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+function LogrosPage4() {
+  return <Typography variant="h1">Pagina 4</Typography>;
+}
+
+export default LogrosPage4;
